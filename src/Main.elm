7fdfee5a6b182c076d6c0132749774bba86ed71a
semzuu <mohamedkhm28+github@gmail.com>
2024-11-10 09:07:45 +0100

    add rendering slides from markdown

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,10 @@ module Main exposing (main)
 
 import Browser
 import Html
+import Html.Attributes
+import Html.Events
+import Markdown
+import Array
 
 main =
   Browser.sandbox
@@ -11,22 +15,68 @@ main =
     }
 
 type alias Model =
-  { name: String
+  { slides: List String
+  , count: Int
+  , index: Int
   }
 
 type Msg
-  = Noop
+  = NextSlide Int Int
+  | PrevSlide Int
 
 init: Model
 init =
-  Model "ma5rout"
+  let
+    slides = List.map String.trim
+      (String.split "---" """
+# First Slide
+Some Text in the first Slide  
+---
+# Second Slide
+Can use images and links  
+[link](https://www.example.com)  
+![cat_image](https://cataas.com/cat)  
+---
+# Third Slide
+Can use <mark>HTML</mark> Tags directly  
+      """)
+  in
+    Model slides (List.length slides) 1
 
 update: Msg -> Model -> Model
 update msg model =
   case msg of
-    Noop ->
-      model
+    NextSlide currIndex count ->
+      { model | index = min count (currIndex + 1) }
+    PrevSlide currIndex ->
+      { model | index = max 1 (currIndex - 1) }
 
 view: Model -> Html.Html Msg
 view model =
-  Html.h1 [] [ Html.text model.name ]
+  Html.div []
+  [ viewControls model
+  , viewSlide model
+  ]
+
+viewSlide: Model -> Html.Html Msg
+viewSlide model =
+  Markdown.toHtmlWith markdownOptions [] (
+    Array.get (model.index - 1) (Array.fromList model.slides)
+    |> Maybe.withDefault "Oops, no slide for you"
+    )
+
+viewControls: Model -> Html.Html Msg
+viewControls model =
+  Html.div [ Html.Attributes.style "background-color" "#ed1818" ]
+  [ Html.button [ Html.Events.onClick (PrevSlide model.index ) ] [ Html.text "Prev" ]
+  , Html.text ("Slide " ++ (String.fromInt model.index) ++ "/" ++ (String.fromInt model.count))
+  , Html.button [ Html.Events.onClick (NextSlide model.index model.count) ] [ Html.text "Next" ]
+  ]
+
+markdownOptions: Markdown.Options
+markdownOptions =
+  { githubFlavored = Nothing
+  , defaultHighlighting = Nothing
+  , sanitize = False
+  , smartypants = True
+  }
